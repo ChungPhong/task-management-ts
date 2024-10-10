914b8bc6321e5b5108362dd64fc59265f013a561
Chung Phong <phonghoang0205@gmail.com>
2024-10-10 09:36:21 +0700

    Lấy ra danh sách công việc

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("ok con gà");
+import express, { Express, Request, Response } from "express";
+import * as database from "./config/database";
+import dotenv from "dotenv";
+import Task from "./model/task.model";
+
+dotenv.config();
+database.connect();
+const app: Express = express();
+const port: number | string = process.env.Port || 3000;
+
+app.get("/tasks", async (req: Request, res: Response) => {
+  const tasks = await Task.find({
+    deleted: false,
+  });
+  res.json(tasks);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
