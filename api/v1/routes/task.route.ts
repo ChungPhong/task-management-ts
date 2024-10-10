902afe1b0ca364b7bbe4761e3ff402a13c0c210b
Chung Phong <phonghoang0205@gmail.com>
2024-10-10 10:08:00 +0700

    Tạo controller

--- api/v1/routes/task.route.ts
+++ api/v1/routes/task.route.ts
@@ -1,26 +1,13 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 const router: Router = Router();
 
-import Task from "../../../model/task.model";
-
+import * as controller from "../controllers/task.controller";
 // const controller = require("../controllers/task.controller");
 // router.get("/", controller.index);
 // router.get("/detail/:id", controller.detail);
 
-router.get("/", async (req: Request, res: Response) => {
-  const tasks = await Task.find({
-    deleted: false,
-  });
-  res.json(tasks);
-});
+router.get("/", controller.index);
 
-router.get("/detail/:id", async (req: Request, res: Response) => {
-  const id: string = req.params.id;
-  const task = await Task.findOne({
-    deleted: false,
-    _id: id,
-  });
-  res.json(task);
-});
+router.get("/detail/:id", controller.detail);
 
 export const taskRoutes: Router = router;
