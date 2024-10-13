9a1281bce40c6bf9110eb57b5b8f3e4f24dd4e01
Chung Phong <phonghoang0205@gmail.com>
2024-10-13 15:17:44 +0700

    Sắp xếp theo tiêu chí (Sort)

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -3,16 +3,30 @@ import { Request, Response } from "express";
 import Task from "../model/task.model";
 
 export const index = async (req: Request, res: Response) => {
-  const find = {
+  //Find
+  interface Find {
+    deleted: boolean;
+    status?: string;
+  }
+  const find: Find = {
     deleted: false,
   };
 
   if (req.query.status) {
-    find["status"] = req.query.status;
+    find.status = req.query.status.toString();
   }
+  //END Find
 
-  const tasks = await Task.find(find);
+  // //Sort
+  const sort = {};
 
+  if (req.query.sortKey && req.query.sortValue) {
+    const sortKey = req.query.sortKey.toString();
+    sort[sortKey] = req.query.sortValue;
+  }
+  // //END Sort
+
+  const tasks = await Task.find(find).sort(sort);
   res.json(tasks);
 };
 
